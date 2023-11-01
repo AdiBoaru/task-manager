4d6733203f8496a01b80e455ba1408431285f9aa
Boaru <adi.boaru@yahoo.com>
2023-11-01 20:24:17 +0200

    up

---
+++ backend/src/main/java/com/togbo/taskmanager/enums/Role.java
@@ -0,0 +1,8 @@
+package com.togbo.taskmanager.enums;
+
+public enum Role {
+    DEVELOPER,
+    PROJECT_MANAGER,
+    TEAM_LEAD,
+    TESTER
+}
--- src/main/java/com/togbo/taskmanager/enums/Role.java
+++
@@ -1,8 +0,0 @@
-package com.togbo.taskmanager.enums;
-
-public enum Role {
-    DEVELOPER,
-    PROJECT_MANAGER,
-    TEAM_LEAD,
-    TESTER
-}
