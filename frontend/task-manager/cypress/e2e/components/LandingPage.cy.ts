7aa2b2329e3bdcef25342d0bd8590a59773dadbb
Flaviu <flaviucisdev@gmail.com>
2023-11-03 12:31:32 +0200

    updated LandingPage tests with login and register links click

--- frontend/task-manager/cypress/e2e/components/LandingPage.cy.ts
+++ frontend/task-manager/cypress/e2e/components/LandingPage.cy.ts
@@ -7,4 +7,20 @@ describe('Testing Landing Page', () => {
     cy.get('[data-testid="landing-page-register-btn"]').should('exist').should('have.text', 'Register Now')
     cy.get('[data-testid="landing-page-title"]').should('exist').should('have.text', 'Task Management For Your Company')
   })
+
+  it('navigates to login when Login btn is clicked', () => {
+    cy.visit('http://localhost:5173/landing-page');
+    cy.viewport(2440, 1920)
+
+    cy.get('[data-testid="landing-page-login-btn"]').should('exist').click();
+    cy.location("pathname").should('equal', '/login');
+  }) 
+
+  it('navigates to register when Register Now btn is clicked', () => {
+    cy.visit('http://localhost:5173/landing-page');
+    cy.viewport(2440, 1920)
+
+    cy.get('[data-testid="landing-page-register-btn"]').should('exist').click();
+    cy.location("pathname").should('equal', '/register');
+  }) 
 })
