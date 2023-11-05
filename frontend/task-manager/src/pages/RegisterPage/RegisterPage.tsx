8111071248cf575bc37445683fb7d0a202421c60
AdiBoaru <34595964+AdiBoaru@users.noreply.github.com>
2023-11-05 16:25:49 +0200

    Revert "updated register page design"

--- frontend/task-manager/src/pages/RegisterPage/RegisterPage.tsx
+++ frontend/task-manager/src/pages/RegisterPage/RegisterPage.tsx
@@ -1,6 +1,3 @@
-import { NavLink } from "react-router-dom";
-
-import { LOGIN } from "../../constants/routePaths";
 import Logo from "../../UI/Logo/Logo";
 import RegisterForm from "../../components/Register/RegisterForm";
 
@@ -9,7 +6,7 @@ const RegisterPage = () => {
     <div className="relative flex justify-between h-screen bg-primaryColor px-[20rem]">
       <div className="flex flex-col w-[70%] justify-between">
         <Logo mt="mt-[150px]" />
-        <div className="py-[3rem] h-[55%] laptop:h-[70%]">
+        <div className="py-[3rem] h-[55%]">
           <RegisterForm />
         </div>
         <p className="w-[13rem] mb-10 text-white laptop:right-[55rem]">
@@ -19,11 +16,11 @@ const RegisterPage = () => {
       <div className="relative w-[30%] flex justify-evenly">
         <div>
           <div className="bg-white h-[500px] w-[2px]" />
-          <div className="bg-white h-[100px] w-[100px] rounded-full absolute left-[25%] laptop:left-[21%]" />
+          <div className="bg-white h-[100px] w-[100px] rounded-full absolute left-[25%]" />
         </div>
         <div>
           <div className="bg-secondaryColor h-[700px] w-[2px]" />
-          <div className="bg-secondaryColor h-[130px] w-[130px] rounded-full absolute left-[56%] laptop:left-[50%]" />
+          <div className="bg-secondaryColor h-[130px] w-[130px] rounded-full absolute left-[56%]" />
         </div>
       </div>
     </div>
