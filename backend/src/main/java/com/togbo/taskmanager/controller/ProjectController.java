ec5ce5daf03737a806a4ce86d7c71722904fab2a
Boaru <adi.boaru@yahoo.com>
2023-11-14 17:13:19 +0200

    update project controller

--- backend/src/main/java/com/togbo/taskmanager/controller/ProjectController.java
+++ backend/src/main/java/com/togbo/taskmanager/controller/ProjectController.java
@@ -6,7 +6,6 @@ import com.togbo.taskmanager.exceptions.ResourceNotFoundException;
 import com.togbo.taskmanager.model.Account;
 import com.togbo.taskmanager.model.Project;
 import com.togbo.taskmanager.services.ProjectService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
