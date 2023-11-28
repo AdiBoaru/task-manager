04129dce6569bc569c66af9a139417199888147d
Boaru <adi.boaru@yahoo.com>
2023-11-28 10:24:19 +0200

    create the jwt authentication filter

---
+++ backend/src/main/java/com/togbo/taskmanager/config/JwtAuthenticationFilterConfig.java
@@ -0,0 +1,25 @@
+package com.togbo.taskmanager.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+//OncePerRequestFilter -> creaza un filtru pentru ficare request
+@Component
+public class JwtAuthenticationFilterConfig extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, //is our request -> we can intercept every request and extract data from it
+            HttpServletResponse response, //is our response -> after we intercept a request we can provide new response with new data
+            FilterChain filterChain //is the chain of responsibility design pattern
+    ) throws ServletException, IOException {
+
+    }
+}
--- backend/src/main/java/com/togbo/taskmanager/model/Employee.java
+++ backend/src/main/java/com/togbo/taskmanager/model/Employee.java
@@ -2,14 +2,19 @@ package com.togbo.taskmanager.model;
 
 import com.togbo.taskmanager.enums.Role;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
 @Table(name = "employees")
-public class Employee {
+public class Employee implements UserDetails {
     /* @Id
      @GeneratedValue
      @Column(columnDefinition = "VARCHAR(36)")
@@ -53,6 +58,43 @@ public class Employee {
         this.birthDate = birthDate;
         this.role = role;
         this.account = account;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        //password should be here
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        //email should be here
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public Long getId() {
@@ -119,7 +161,6 @@ public class Employee {
     public void setAccount(Account account) {
         this.account = account;
     }
-
 
     @Override
     public String toString() {
