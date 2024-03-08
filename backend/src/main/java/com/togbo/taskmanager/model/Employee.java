067dc047af2d9c13f105d8e14b56c7622632b8e1
Adi <adi.boaru@yahoo.com>
2024-03-08 11:08:32 +0200

    eno

--- backend/src/main/java/com/togbo/taskmanager/model/Employee.java
+++ backend/src/main/java/com/togbo/taskmanager/model/Employee.java
@@ -15,6 +15,8 @@ import java.util.Set;
 @Entity
 @Table(name = "employees")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "first_name")
     private String firstName;
