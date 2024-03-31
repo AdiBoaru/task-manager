d3e75c2c9217ec4052f4409b44f2fbfebdef17ae
Adi <adi.boaru@yahoo.com>
2024-03-31 17:47:13 +0300

    configure webSocket, establish connection and send message

--- backend/src/main/java/com/togbo/taskmanager/config/CorsConfig.java
+++ backend/src/main/java/com/togbo/taskmanager/config/CorsConfig.java
@@ -2,15 +2,17 @@ package com.togbo.taskmanager.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/project/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/**") // Allow all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
+                .allowedHeaders("*"); // Allowed headers
     }
 }
---
+++ backend/src/main/java/com/togbo/taskmanager/controller/WebSocketController.java
@@ -0,0 +1,22 @@
+package com.togbo.taskmanager.controller;
+
+
+import com.togbo.taskmanager.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+public class WebSocketController {
+
+    @MessageMapping("/application")
+    @SendTo("/all/messages")
+    public Message send(final Message message) throws Exception {
+        return message;
+    }
+}
